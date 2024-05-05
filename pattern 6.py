1ad8c368971b30c444ac9e86e6e7b0d1d763054d
Dharshini0611 <140406453+Dharshini0611@users.noreply.github.com>
2024-05-05 20:13:05 +0530

    Add files via upload

---
+++ pattern 6.py
@@ -0,0 +1,9 @@
+a=1
+end=2
+n=4
+for i in range(n):
+    for m in range(1,end):
+        print(a,end=" ")
+        a+=1
+    print(" ")
+    end+=2
---
+++ pattern 7.py
@@ -0,0 +1,5 @@
+n=8
+for r in range(1,n):
+    for m in range(r,0,-1):
+        print(m,end=" ")
+    print(" ")
